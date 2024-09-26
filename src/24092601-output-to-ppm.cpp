754274ad6e8b38b2ea0648e2936704cce530813d
TheToror <thetoror@sjtu.edu.cn>
2024-09-26 23:20:00 +0800

    feat: output an image with progress log

--- src/24092601-output-to-ppm.cpp
+++ src/24092601-output-to-ppm.cpp
@@ -11,20 +11,20 @@ std::ostream &operator<<(std::ostream &os,const Colf &c){
 }
 typedef Colf Shader(int x,int y);
 void render(int h,int w,Shader *shader,std::vector<Colf> &buf){
-  // std::cerr << "h=" << h << " w=" << w << '\n';
-  for(int j=h-1;j>=0;j--){
-    for(int i=0;i<w;i++){
-      // std::cerr << "at " << i << ' ' << j << '\n';
+  for(int j=0;j<h;j++){
+    std::clog << "\rRendering progress: " << double(j)/h << "%        " << std::flush;
+    for(int i=0;i<w;i++)
       buf.push_back(shader(i,j));
-    }
   }
+  std::clog << "\rRendering progress: 100%        ";
+  std::clog << "\nDone." << std::endl;
 }
 int main(){
-  static const int w=480,h=400;
+  static const int w=256,h=256;
   std::ofstream ofs("../image/demo.ppm");
   ofs << "P3\n" << w << ' ' << h << "\n255\n";
   std::vector<Colf> buf;
-  render(h,w,[](int i,int j){return Colf(i/float(w),j/float(h),0.25);},buf);
+  render(h,w,[](int i,int j){return Colf(i/float(w),j/float(h),0.);},buf);
   for(auto &c:buf)ofs << c << '\n';
   return 0;
 }
