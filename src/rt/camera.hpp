6358adfae68976d83df5f57c96b0003bb7c12493
TheToror <thetoror@sjtu.edu.cn>
2024-09-29 14:08:20 +0800

    feat: basic camera

---
+++ src/rt/camera.hpp
@@ -0,0 +1,42 @@
+#ifndef __CAMERA_H__
+#define __CAMERA_H__
+#include<cmath>
+#include"ray.hpp"
+class camera{
+ private:
+  point at,tar,up;
+  int iw,ih;double fov;
+  double asr,vph,vpw;
+  char done;
+ public:
+  camera():done(7){}
+  camera& setDire(const point& at,const point& tar,const vec3d& up){
+    this->at=at;
+    this->tar=tar;
+    this->up=up;
+    done|=1;
+    return *this;
+  }
+  camera& setRes(int iw,int ih){
+    this->iw=iw;
+    this->ih=ih;
+    asr=double(iw)/ih;
+    done|=2;
+    if(done&4){
+      vph=2*tan(fov/2);
+      vpw=vph*asr;
+    }
+    return *this;
+  }
+  camera& setFov(double fov){
+    this->fov=fov;
+    done|=4;
+    if(done&2){
+      vph=2*tan(fov/2);
+      vpw=vph*asr;
+    }
+    return *this;
+  }
+  bool ready()const{return done==7;}
+};
+#endif
