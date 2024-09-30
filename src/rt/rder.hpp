49cd8865c94dfb3a381eb06a65242810ff8278ba
TheToror <thetoror@sjtu.edu.cn>
2024-09-30 23:18:02 +0800

    feat: first sphere

--- src/rt/rder.hpp
+++ src/rt/rder.hpp
@@ -3,10 +3,20 @@
 #include<vector>
 #include"color.hpp"
 #include"camera.hpp"
+bool hit_sphere(const Vec3d& center,double radius,const Ray& r){
+  Vec3d oc=center-r.getOri();
+  auto a=r.getDir()*r.getDir();
+  auto b=-2.*r.getDir()*oc;
+  auto c=oc*oc-radius*radius;
+  auto Del=b*b-4*a*c;
+  return (Del>=0);
+}
 class rder{
  private:
   Camera cam;
   Col trace(Ray r)const{
+    if(hit_sphere(Vec3d(0,0,-1),1,r))
+      return Col(0,.5,0);
     Vec3d ud=r.getDir().norm();
     double t=0.5*(ud.y()+1.);
     return Col(1,1,1)*(1-t)+Col(0.5,0.7,1)*t;
