e444a40b6ac494b2624102ded39e07fe563cdaf0
TheToror <thetoror@sjtu.edu.cn>
2024-10-01 23:48:45 +0800

    fix: positive check for radius

--- src/rt/sphere.hpp
+++ src/rt/sphere.hpp
@@ -7,7 +7,7 @@ class Sphere:public Hittable{
   Point center;
   double radius;
  public:
-  Sphere(const Point &center,const double radius):center(center),radius(radius){}
+  Sphere(const Point &center,const double radius):center(center),radius(fmax(0.,radius)){}
   virtual bool hit(const Ray &ray,double tmin,double tmax,Hitment &h)const override{
     Vec3d oc=center-ray.getOri();
     const Vec3d &rd=ray.getDir();
@@ -16,13 +16,12 @@ class Sphere:public Hittable{
     double c=oc.lenSqr()-radius*radius;
     double delD4=hlf*hlf-a*c;
     if(delD4<0)return false;
-    double root=(hlf-sqrt(delD4))/a;
+    double &root=h.t=(hlf-sqrt(delD4))/a;
     if(root<tmin||root>tmax){
       root=(hlf+sqrt(delD4))/a;
       if(root<tmin||root>tmax)
         return false;
     }
-    h.t=root;
     h.p=ray.at(root);
     h.n=(h.p-center)/radius;
     return true;
