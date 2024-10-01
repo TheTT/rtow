14a2c158d679fc047a57613274c7f23c1b206977
TheToror <thetoror@sjtu.edu.cn>
2024-10-01 22:31:07 +0800

    feat: basic hittable

---
+++ src/rt/hittable.hpp
@@ -0,0 +1,14 @@
+#ifndef __HITTABLE_HPP__
+#define __HITTABLE_HPP__
+#include"ray.hpp"
+struct Hitment{
+  Point p;
+  Vec3d n;
+  double t;
+};
+class Hittable{
+ public:
+  ~Hittable()=default;
+  virtual bool hit(const Ray &r,double tmin,double tmax,Hitment &h)const=0;
+};
+#endif
