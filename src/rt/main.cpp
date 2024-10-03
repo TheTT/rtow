f431068581e3131fba3598cd93818c935f0c39d3
TheToror <thetoror@sjtu.edu.cn>
2024-10-03 22:57:12 +0800

    fix: add concepts to Scene::add

--- src/rt/main.cpp
+++ src/rt/main.cpp
@@ -6,10 +6,24 @@
 // typedef Col Shader(int x,int y);
 int main(){
   static const int w=256,h=256;
+  auto sph1=Sphere(Vec3d(0,0,-1),1);
+  auto sph2=Sphere(Vec3d(0,-100.5,-1),100);
   std::ofstream ofs("../../image/demo.ppm");
   ofs << "P3\n" << w << ' ' << h << "\n255\n";
   std::vector<Col> buf;
-  tracer().setCam(Camera().setDire(Point(0,0,1),Point(0,0,0),Vec3d(0,1,0)).setRes(w,h).setFov(90)).setScn(Scene().add(std::make_shared<Sphere>(Vec3d(0,0,-1),1)).add(std::make_shared<Sphere>(Vec3d(0,-100.5,-1),100))).work(buf);
+  tracer()
+  .setCam(
+    Camera()
+    .setDire(Point(0,0,1),Point(0,0,0),Vec3d(0,1,0))
+    .setRes(w,h)
+    .setFov(90)
+  )
+  .setScn(
+    Scene()
+    .add(sph1)
+    .add(sph2)
+  )
+  .work(buf);
   for(auto &c:buf)ofs << c << '\n';
   return 0;
 }
--- src/rt/scene.hpp
+++ src/rt/scene.hpp
@@ -3,6 +3,9 @@
 #include<vector>
 #include<memory>
 #include"hittable.hpp"
+#include"sphere.hpp"
+template <typename _Tp>
+concept DerivedFromHittable=std::is_base_of_v<Hittable,_Tp>;
 class Scene{
  private:
   std::vector<std::shared_ptr<Hittable> > objs;
@@ -12,14 +15,16 @@ class Scene{
     objs.push_back(obj);
     return *this;
   }
-  // inline Scene &add(Hittable &&obj){
-  //   objs.push_back(std::make_shared<Hittable>(std::move(obj)));
-  //   return *this;
-  // }
-  // inline Scene &add(const Hittable &obj){
-  //   objs.push_back(std::make_shared<Hittable>(obj));
-  //   return *this;
-  // }
+  template <DerivedFromHittable _Tp>
+  inline Scene &add(_Tp &&obj){
+    objs.push_back(std::make_shared<_Tp>(std::move(obj)));
+    return *this;
+  }
+  template <DerivedFromHittable _Tp>
+  inline Scene &add(const _Tp &obj){
+    objs.push_back(std::make_shared<_Tp>(obj));
+    return *this;
+  }
   bool hit(const Ray &ray,double tmin,double tmax,Hitment &h)const{
     Hitment tmp;
     bool hit=false;
