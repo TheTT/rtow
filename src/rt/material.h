17414f52debd395dd6381ad22d698becc1e8e1f7
TheToror <thetoror@sjtu.edu.cn>
2024-10-29 12:16:47 +0800

    feat: make a new dielectric (todo)

--- src/rt/material.h
+++ src/rt/material.h
@@ -26,9 +26,16 @@ class Metal:public Material{
 };
 class Nuclear:public Material{
  private:
+  double density;
+ public:
+  Nuclear(double density=1.);
+  virtual bool backward(const Ray& rin,const Hitment& hit,Col& att,Ray& rout)const override;
+};
+class Dielectric:public Material{
+ private:
   double refri;
  public:
-  Nuclear(double refri=1.);
+  Dielectric(double refri=1.);
   virtual bool backward(const Ray& rin,const Hitment& hit,Col& att,Ray& rout)const override;
 };
 #endif
